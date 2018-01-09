98b56a96c37b2e496ad5c5313967320da7217c5e
JeffDowns <jeffdowns@jeffdowns.net>
2018-01-09 13:03:34 -0500

    Seperated game from standalone website. Future Plan: Going to implement login to play game.

---
+++ pages/game.php
@@ -0,0 +1,9 @@
+   <?php
+include "../templates/header.php";
+?>
+   <canvas id="graphics" width="1200" height="800"></canvas>
+   <audio id="themeMusic" src="../game/assets/audio/theme.mp3"></audio>
+   <script src="../game/main.js"></script>                 
+<?php
+include "../templates/footer.php";
+?>
